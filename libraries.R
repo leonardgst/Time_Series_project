92bc39f688404e087bcfe10d385e164fe26c3187
Léonard Gousset <goussetleonard@gmail.com>
2023-10-22 12:02:13 +0200

    j'ai oublié ça

--- libraries.R
+++ libraries.R
@@ -1,2 +1,23 @@
+##########################################################
+##########################################################
+##                                                      ##
+##                   EXPLORATION FILE                   ##
+##                                                      ##
+##########################################################
+##########################################################
+
+#### LIBRARIES ####
+
 library(dplyr)
 library(ggplot2)
+
+
+#### DATASET IMPORT ####
+# ad_viz_plotval_data_2021.csv = Carbonne dans New-York depuis 2021
+#aqs_sites.csv = jspatrop
+
+test <- rbind(read.csv("ad_viz_plotval_data_2021.csv"),
+              read.csv("ad_viz_plotval_data_2022.csv"),
+              read.csv("ad_viz_plotval_data_2023.csv"))
+
+
